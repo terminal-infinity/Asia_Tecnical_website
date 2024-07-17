5487477afb7fef9a66d26adbc76ee3a6449d10ea
Nur Jannat <nurjannat200029@gmail.com>
2024-07-17 20:15:27 +0600

    Multi-Authenication and Admin Panel setup

---
+++ resources/views/admin/partials/scripts.blade.php
@@ -0,0 +1,17 @@
+<!-- core:js -->
+<script src="{{ asset('backend/assets/vendors/core/core.js') }}"></script>
+<!-- endinject -->
+
+<!-- Plugin js for this page -->
+<script src="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
+<script src="{{ asset('backend/assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+<!-- End plugin js for this page -->
+
+<!-- inject:js -->
+<script src="{{ asset('backend/assets/vendors/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/template.js') }}"></script>
+<!-- endinject -->
+
+<!-- Custom js for this page -->
+<script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
+<!-- End custom js for this page -->
